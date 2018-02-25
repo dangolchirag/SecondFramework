1d138c3382f8745ca691c78ba32b9a7ab3aa6aa5
Inficare Pvt. Ltd <inficare@Inficares-Mac-mini.local>
2018-02-25 15:50:22 +0545

    initial commit

---
+++ SecondFramework/SecondFramework/SecondFramework.h
@@ -0,0 +1,19 @@
+//
+//  SecondFramework.h
+//  SecondFramework
+//
+//  Created by Inficare Pvt. Ltd. on 25/02/2018.
+//  Copyright © 2018 Inficare Pvt. Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SecondFramework.
+FOUNDATION_EXPORT double SecondFrameworkVersionNumber;
+
+//! Project version string for SecondFramework.
+FOUNDATION_EXPORT const unsigned char SecondFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SecondFramework/PublicHeader.h>
+
+
---
+++ SecondFramework/SecondFramework/Utils.swift
@@ -0,0 +1,16 @@
+//
+//  Utils.swift
+//  SecondFramework
+//
+//  Created by Inficare Pvt. Ltd. on 25/02/2018.
+//  Copyright © 2018 Inficare Pvt. Ltd. All rights reserved.
+//
+
+import Foundation
+class Utils {
+    
+    init(){}
+    public static func printLogMessage(){
+        print("Log message ---> Connected")
+    }
+}
